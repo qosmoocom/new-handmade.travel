b5d4a20f3bab5e24986736c5ef581e404d6f4002
Abdurahmon216 <abdurahmonziyodov216@gmail.com>
2022-01-31 18:14:18 +0500

    aded

--- app/app/header/header.js
+++ app/app/header/header.js
@@ -4,26 +4,31 @@ import Image from "next/image";
 import Text from "../../components/Text";
 import { IoMdCall } from "react-icons/io";
 import { BsTelegram, BsFacebook, BsTwitter } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+
 export default function Header() {
   const { getItem } = useContext(AppContext);
 
   return (
     <div id="header" className="header">
-      {/* mobile menu items */}
-      <div className="header_nav__menu_mobile">
+      {/* mobile menu  */}
+      <AiOutlineMenu className="mobile-menu-icon" />
+
+      <div className="header_nav_menu_mobile">
         {getItem("head_menu_title", "menu").map((menuItem, index) => (
-          <div className="header_nav__menu_item" key={index}>
+          <div className="header_nav_menu_item" key={index}>
             <Text name={"head_menu_title"} group={"menu"}>
               {menuItem?.head_menu_title}
             </Text>
           </div>
         ))}
       </div>
+      {/* mobile menu  */}
       {/* header  */}
       <div className="header_navbar">
         {/* navbar logo */}
-        <div className="header_navbar__logo">
-          <div className="header_navbar__logo_box">
+        <div className="header_navbar_logo">
+          <div className="header_navbar_logo_box">
             <Image
               src={getItem("my_logo_img_url")}
               width={140}
@@ -31,17 +36,17 @@ export default function Header() {
               priority
             />
           </div>
-          <div className="header_navbar__logo_text" data-name="my_logo_text">
+          <div className="header_navbar_logo_text" data-name="my_logo_text">
             <Text name={"my_logo_text"}>{getItem("my_logo_text")}</Text>
           </div>
         </div>
         {/* /navbar logo */}
-        <div className="header_navbar__nav">
-          <div className="header_nav__box">
+        <div className="header_navbar">
+          <div className="header_nav_box">
             {/* navbar menu */}
-            <div className="header_nav__menu">
+            <div className="header_nav_menu">
               {getItem("head_menu_title", "menu").map((menuItem, index) => (
-                <div className="header_nav__menu_item" key={index}>
+                <div className="header_nav_menu_item" key={index}>
                   <Text name={"head_menu_title"} group={"menu"}>
                     {menuItem?.head_menu_title}
                   </Text>
@@ -51,50 +56,52 @@ export default function Header() {
 
             {/* /navbar menu */}
             {/* navbar texts box */}
-            <div className="header_nav__texts_box">
-              <div className="header_nav__middle_text ">
+            <div className="header_texts_box">
+              <div className="header_middle_text ">
                 <Text name={"head_text_1"}>{getItem("head_text_1")}</Text>
               </div>
-              <div className="header_nav__big_text">
+              <div className="header_big_text">
                 <Text name={"head_text_2"}>{getItem("head_text_2")}</Text>
               </div>
-              <div className="header_nav__small_text">
+              <div className="header_small_text">
                 <Text name={"head_text_3"}>{getItem("head_text_3")}</Text>
               </div>
             </div>
             {/* /navbar texts box */}
           </div>
         </div>
-        <div className="header_navbar__right-box">
-          <div className="header_navbar__phone">
-            <IoMdCall className="header_navbar__phone-icon" />
+        {/* navbar right items */}
+        <div className="header_navbar_right-box">
+          <div className="header_navbar_phone">
+            <IoMdCall className="header_navbar_phone-icon" />
             <a href={`tel:+${getItem("head_phone_href")}`}>
               <Text name={"head_phone_ru"}>{getItem("head_phone_ru")}</Text>
             </a>
           </div>
-          <div className="header_navbar__social-m">
-            <div className="header_navbar__social-m-item telegram-icon">
+          <div className="header_navbar_social-m">
+            <div className="header_navbar_social-m-item telegram-icon">
               <a href={`tel:+${getItem("head_tg_href")}`}>
-                <BsTelegram className="header_navbar__telegram-icon" />
+                <BsTelegram className="header_navbar_telegram-icon" />
               </a>
             </div>
             {/* BsTwitter */}
-            <div className="header_navbar__social-m-item facebook-icon">
+            <div className="header_navbar_social-m-item facebook-icon">
               <a href={`tel:+${getItem("head_fc_href")}`}>
-                <BsFacebook className="header_navbar__facebook-icon" />
+                <BsFacebook className="header_navbar_facebook-icon" />
               </a>
             </div>
 
-            <div className="header_navbar__social-m-item twitter-icon">
+            <div className="header_navbar_social-m-item twitter-icon">
               <a href={`tel:+${getItem("head_fc_href")}`}>
-                <BsTwitter className="header_navbar__twitter-icon" />
+                <BsTwitter className="header_navbar_twitter-icon" />
               </a>
             </div>
           </div>
         </div>
+        {/* /navbar right items */}
       </div>
       <div className="header_main">
-        <div className="header_main__BGI">
+        <div className="header_main_bgi">
           <Image
             width={1200}
             height={500}
@@ -106,7 +113,7 @@ export default function Header() {
             priority
           />
         </div>
-        <div className="header_main__BGI_mobile">
+        <div className="header_main_bgi_mobile">
           <Image
             width={350}
             height={290}
