481b87468e989c47f9fd6e28663d22e57026c9d9
Abdurahmon216 <abdurahmonziyodov216@gmail.com>
2022-02-12 11:30:13 +0500

    added

--- app/app/rate/rate.js
+++ app/app/rate/rate.js
@@ -1,4 +1,6 @@
 import { BsExclamationCircle, BsCheck2 } from "react-icons/bs";
+import { IoIosMail, IoMdCall } from "react-icons/io";
+import { RiMessageFill } from "react-icons/ri";
 import React, { useContext, useState } from "react";
 
 import Text from "../../components/Text";
@@ -6,11 +8,12 @@ import Item from "../../components/Item";
 
 import { AppContext } from "..";
 
-function DataIs({ isOpen = false }) {
+function DataIs({ isOpen = false, tariffNum = "" }) {
   const { getItem } = useContext(AppContext);
   return (
     isOpen &&
     getItem("rateData", "rateData").map((item, index) => {
+      const sizeData = getItem("rateData", "rateData").length;
       return (
         <Item
           key={index}
@@ -28,7 +31,19 @@ function DataIs({ isOpen = false }) {
               {item.rate_head}
               <BsExclamationCircle className="excl" />
             </Text>
-            <BsCheck2 className="bs_check" />
+
+            {index === sizeData - 1 ? (
+              <div className="last-icons">
+                <IoIosMail className="icon active" />
+                <RiMessageFill className="icon active" />
+                <IoMdCall
+                  className={`icon ${tariffNum === 2 ? "active" : ""}`}
+                />
+              </div>
+            ) : (
+              <BsCheck2 className="bs_check" />
+            )}
+
             <p className="rate-list-item-descript">
               <Text
                 name="rate_title"
@@ -80,6 +95,9 @@ export default function Rate() {
                         </Text>
                       </span>
                       <span className="btn-item btn-right">
+                        <Text name="rate_ot" itIsClassName="btn-item btn-right">
+                          {getItem("rate_ot")}{" "}
+                        </Text>
                         <Text
                           name="rate_money"
                           group="rate_name_data"
@@ -93,6 +111,7 @@ export default function Rate() {
                     <div className="header-list-modal">
                       <DataIs
                         isOpen={toggle.isOpen && index === toggle.index}
+                        tariffNum={index}
                       />
                     </div>
                   </div>
@@ -100,8 +119,39 @@ export default function Rate() {
               }
             )}
           </div>
-          <div className="rate-list">
-            <DataIs />
+
+          {/* --------- */}
+          <div className="rate-header-list-table">
+            <div className="list-table-tr">
+              <div className="list-table-th">{getItem("rate_name")}</div>
+              {getItem("rate-header-list", "rate_name_data").map(
+                (item, index) => {
+                  return (
+                    <div className="list-table-th">
+                      <Text
+                        name="rate_name_head"
+                        group="rate_name_data"
+                        id={index}
+                        itIsClassName="btn-item btn-left"
+                      >
+                        {item.rate_name_head}
+                      </Text>
+                      <Text name="rate_ot" itIsClassName="btn-item btn-right">
+                        {getItem("rate_ot")}{" "}
+                      </Text>
+                      <Text
+                        name="rate_money"
+                        group="rate_name_data"
+                        id={index}
+                        itIsClassName="btn-item btn-right"
+                      >
+                        {item.rate_money}
+                      </Text>
+                    </div>
+                  );
+                }
+              )}
+            </div>
           </div>
         </div>
       </div>
