ca5ba8cd497ebdafa6acedc27c43662fa968e4c9
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-03-07 10:55:22 +0500

    edit

---
+++ backend/routes/tour.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createOne,
+  getItem,
+  getItems,
+  updateOne,
+  deleteOne,
+  getItemByUser,
+  getItemsUser,
+} = require("../controllers/tourController");
+const { protect, admin, moderator } = require("../middleware/auth");
+
+router.post("/add", protect, moderator, createOne);
+router.get("/:tour_id/:id", getItemsUser);
+router.get("/all", protect, admin, getItems);
+router.get("/:id", protect, moderator, getItem);
+router.put("/:id", protect, moderator, updateOne);
+router.get("/byUser/:userID", protect, moderator, getItemByUser);
+router.delete("/:id", protect, admin, deleteOne);
+
+module.exports = router;
