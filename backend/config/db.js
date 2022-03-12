27edaffbda31dd1743350ff62f2dc5c349c2234e
Gulxayo2804 <gulxayo02@mail.ru>
2022-03-12 14:23:39 +0500

    add localniy

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
-const dbUri =
-  "mongodb+srv://admin:admin2022@cluster0.7xf4q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const dbUri =
+//   "mongodb+srv://admin:admin2022@cluster0.7xf4q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const dbUri =mkd;
+const dbUri = "mongodb://localhost:27017/handemade";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(dbUri, {
