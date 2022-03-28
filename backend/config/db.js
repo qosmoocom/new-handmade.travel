6691e3f2156478c3bf7d477f0dcb511f91c7775a
Abdurahmon216 <abdurahmonziyodov216@gmail.com>
2022-03-28 12:24:34 +0500

    ssss

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const dbUri = "mongodb+srv://anvar:1DRLK4PrhRd9FcRd@cluster0.uoe1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//const dbUri = "mongodb+srv://anvar:1DRLK4PrhRd9FcRd@cluster0.uoe1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 // const dbUri =mkd;
-// const dbUri = "mongodb://localhost:27017/handemade";
+ const dbUri = "mongodb://localhost:27017/handemade";
 
 const connectDB = async () => {
   try {
