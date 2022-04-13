7ea67fac03533dbe83082fa50499eb4c349840d9
Abdurahmon216 <abdurahmonziyodov216@gmail.com>
2022-04-13 15:21:04 +0500

    ssss

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // const dbUri =
-//   "mongodb+srv://anvar:1DRLK4PrhRd9FcRd@cluster0.uoe1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// "mongodb+srv://anvar:1DRLK4PrhRd9FcRd@cluster0.uoe1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 // const dbUri =mkd;
 const dbUri = "mongodb://localhost:27017/handmade";
