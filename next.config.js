e8704d164d4640b2789864fdc6e1424768111d96
Abdurahmon216 <abdurahmonziyodov216@gmail.com>
2022-05-29 11:39:27 +0500

    ssss

--- next.config.js
+++ next.config.js
@@ -14,12 +14,12 @@ module.exports = {
       script: "backend/server.js",
       watch: true,
       env: {
-        PORT: 3012,
+        PORT: 3005,
         // 3005
         NODE_ENV: "development",
       },
       env_production: {
-        PORT: 3012,
+        PORT: 3005,
         // 3005
         NODE_ENV: "production",
       },
