a38839e6e600f2250be33bf4610f9dfb4fc256aa
Abdurahmon216 <abdurahmonziyodov216@gmail.com>
2022-05-29 15:52:05 +0500

    sss

--- app/components/Image.js
+++ app/components/Image.js
@@ -39,12 +39,13 @@ export default function Index({
   parentId = "",
   childId = "",
   priority,
-  sectionName
+  sectionName,
 }) {
   const { isEdit: isAdmin } = useSelector((st) => st.admin);
   const dispatch = useDispatch();
   const [src, setSrc] = useState("");
-  
+  const [img, setImg] = useState(false);
+
   useEffect(() => {
     setSrc(imgSrc);
   }, [imgSrc]);
@@ -67,73 +68,78 @@ export default function Index({
           childId,
           list,
           name,
-          sectionName
+          sectionName,
         },
       });
     }
   };
 
+  setTimeout(() => {
+    setImg(true);
+  }, [2000]);
+
   const myLoader = ({ src, width, quality }) => {
     return `https://okeey.uz/${src}?w=${width}&q=${quality || 75}`;
   };
-  
-  if (layout === "fill" && src) {
-    return (
-      <>
-        {isAdmin ? (
-          <img
-            src={src}
-            alt={alt || ""}
-            title={title || ""}
-            onDoubleClick={(e)=>doubleClickHandler(e)}
-            style={{ width: "100%", height: "100%", objectFit: objectFit }}
-          />
-        ) : (
-          <Image
-            loader={myLoader}
-            src={src}
-            alt={alt || ""}
-            title={title || ""}
-            priority={priority === "important"}
-            objectFit={objectFit}
-            layout={layout}
-            onDoubleClick={doubleClickHandler}
-            loading="eager"
-          />
-        )}
-      </>
-    );
-  }
 
-  
-  return (
-    <Wrapper>
-      {src && (
+  if (img) {
+    if (layout === "fill" && src) {
+      return (
         <>
           {isAdmin ? (
             <img
-              onDoubleClick={(e)=>doubleClickHandler(e)}
-              style={{ width: "100%", height: "100%", objectFit: objectFit }}
               src={src}
+              alt={alt || ""}
+              title={title || ""}
+              onDoubleClick={(e) => doubleClickHandler(e)}
+              style={{ width: "100%", height: "100%", objectFit: objectFit }}
             />
           ) : (
             <Image
               loader={myLoader}
               src={src}
-              key={src}
-              width={width}
-              height={height}
-              onDoubleClick={doubleClickHandler}
-              alt={alt}
-              title={title}
+              alt={alt || ""}
+              title={title || ""}
               priority={priority === "important"}
               objectFit={objectFit}
               layout={layout}
+              onDoubleClick={doubleClickHandler}
               loading="eager"
             />
           )}
         </>
-      )}
-    </Wrapper>
-  );
+      );
+    }
+
+    return (
+      <Wrapper>
+        {src && (
+          <>
+            {isAdmin ? (
+              <img
+                onDoubleClick={(e) => doubleClickHandler(e)}
+                style={{ width: "100%", height: "100%", objectFit: objectFit }}
+                src={src}
+              />
+            ) : (
+              <Image
+                loader={myLoader}
+                src={src}
+                key={src}
+                width={width}
+                height={height}
+                onDoubleClick={doubleClickHandler}
+                alt={alt}
+                title={title}
+                priority={priority === "important"}
+                objectFit={objectFit}
+                layout={layout}
+                loading="eager"
+              />
+            )}
+          </>
+        )}
+      </Wrapper>
+    );
+  } else return <div></div>;
 }
