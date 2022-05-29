ce1df8911dd9b30a83721279c8d9ad4c31cc9979
Abdurahmon216 <abdurahmonziyodov216@gmail.com>
2022-05-29 17:45:47 +0500

    c

--- app/app/footer/index.js
+++ app/app/footer/index.js
@@ -738,7 +738,7 @@ const MyModal = styled.div`
     height: 100%;
     overflow: hidden;
     position: relative;
-    
+
     .slide-container {
       position: relative;
       height: 100%;
@@ -772,7 +772,7 @@ const PDF_MODAL = styled.div`
     left: 0;
   }
   .modal_content {
-    height: 100vh; 
+    height: 100vh;
     /* width: 100vw; */
     width: 100vw;
     position: relative;
@@ -1450,96 +1450,6 @@ export default function index({ footer }) {
           </div>
         </Full_screen>
       </div>
-
-      <Script
-        id="yandexx"
-        // strategy="afterInteractive"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-                (function () {
-              'use strict';
-
-              // Флаг, что Метрика уже загрузилась.
-              var loadedMetrica = false,
-
-                  // Переменная для хранения таймера.
-                  timerId;
-
-              // Для бота Яндекса грузим Метрику сразу без "отложки",
-              // чтобы в панели Метрики были зелёные кружочки
-              // при проверке корректности установки счётчика.
-              if (navigator.userAgent.indexOf('YandexMetrika') > -1) {
-                  loadMetrica();
-              } else {
-                  window.addEventListener('scroll', loadMetrica, { passive: true });
-
-                  window.addEventListener('touchstart', loadMetrica);
-
-                  document.addEventListener('mouseenter', loadMetrica);
-
-                  document.addEventListener('click', loadMetrica);
-
-                  document.addEventListener('DOMContentLoaded', loadFallback);
-              }
-
-              function loadFallback() {
-                  timerId = setTimeout(loadMetrica, 1000);
-              }
-
-              function loadMetrica(e) {
-
-                  if (e && e.type) {
-                    console.log(e.type);
-                  } else {
-                    console.log('DOMContentLoaded');
-                  }
-
-                  if (loadedMetrica) {
-                    return;
-                  }
-
-                  (function (m, e, t, r, i, k, a) {
-                    m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-                    m[i].l = 1 * new Date(); k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-                  })
-                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-                  ym(${getItem("app_yandex_metirka_id")}, "init", {
-                    clickmap: true,
-                    trackLinks: true,
-                    accurateTrackBounce: true,
-                    webvisor: true
-                  });
-
-
-              
-            {/* <noscript><div><img src="https://mc.yandex.ru/watch/86955280" style="position:absolute; left:-9999px;" alt="" /></div></noscript> */}
-
-
-                  !function (f, b, e, v, n, t, s) { if (f.fbq) return; n = f.fbq = function () { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments) }; if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0; t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s) }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '4544213979029989'); fbq('track', 'PageView');
-
-                  // (function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'}); 
-                  //       var f = d.getElementsByTagName(s)[0],
-                  //           j = d.createElement(s),
-                  //           dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-                  // })(window, document, 'script', 'dataLayer', 'GTM-MX4JKC7')
-
-
-                  loadedMetrica = true;
-
-                  clearTimeout(timerId);
-
-                  window.removeEventListener('scroll', loadMetrica);
-                  window.removeEventListener('touchstart', loadMetrica);
-                  document.removeEventListener('mouseenter', loadMetrica);
-                  document.removeEventListener('click', loadMetrica);
-                  document.removeEventListener('DOMContentLoaded', loadFallback);
-              }
-            })()
-              `,
-        }}
-      />
     </>
   );
 }
