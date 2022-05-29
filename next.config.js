4e6823521380059a297b763e35dda62499359764
Abdurahmon216 <abdurahmonziyodov216@gmail.com>
2022-05-29 10:59:02 +0500

    sss

--- next.config.js
+++ next.config.js
@@ -25,4 +25,18 @@ module.exports = {
       },
     },
   ],
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|webp)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
