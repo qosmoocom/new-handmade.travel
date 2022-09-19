b746945404196a5c09e9de0268cd3257e612db64
Anvar Turakulov <anvarsam@mail.ru>
2022-09-19 09:37:11 +0500

    s

--- pages/editor.js
+++ pages/editor.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import Header from '../app/home/header'
 import Footer from '../app/home/footer'
 import dynamic from 'next/dynamic';
-import {homeRu} from '../store/data/home/homeRu'
+// import {homeRu} from '../store/data/home/homeRu'
 // import {offerRu} from '../store/data/offer/offerRu'
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import 'react-quill/dist/quill.snow.css';
@@ -30,7 +30,7 @@ const OfferPage = () => {
 
   return (
     <>
-      <Header data={homeRu} />
+      {/* <Header data={homeRu} />
       <Section>
         <div className="container">
           <ReactQuill theme="snow" value={value} onChange={setValue} modules={modules}/>
@@ -38,7 +38,7 @@ const OfferPage = () => {
         </div>
         
       </Section>
-      <Footer data={homeRu}/>
+      <Footer data={homeRu}/> */}
     </>
     
   )
